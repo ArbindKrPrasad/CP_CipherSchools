6af17efc18a53de53fcc206c402d59ff56c11c11
ArbindKrPrasad <arbind.kr25801@gmail.com>
2021-04-08 17:06:05 +0530

    Commit

--- Assignment2/printAllPermutation.cpp
+++ Assignment2/printAllPermutation.cpp
@@ -1,20 +1,39 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-void printPermutation(string str, int cl,int l){
+// void printPermutation(string str, int cl,int l){
+//     if(cl == l){
+//         cout<<str<<endl;
+//         return;
+//     }
+//     for(int i=cl; i<=l; i++){
+//         swap(str[cl], str[i]);
+//         printPermutation(str, cl+1, l);
+//         swap(str[cl], str[i]);
+//     }
+
+// }
+
+void printPermutation(vector<string> vec, int cl,int l){
     if(cl == l){
-        cout<<str<<endl;
+        for(int i=0; i<vec.size(); i++){
+            if(i==vec.size()-1) cout<<vec[i];
+            else cout<<vec[i]<<" > ";
+        }
+        cout<<endl;
         return;
     }
     for(int i=cl; i<=l; i++){
-        swap(str[cl], str[i]);
-        printPermutation(str, cl+1, l);
-        swap(str[cl], str[i]);
+        swap(vec[cl], vec[i]);
+        printPermutation(vec, cl+1, l);
+        swap(vec[cl], vec[i]);
     }
 
 }
 
 int main(){
-    printPermutation("ABC", 0, 2);
+    vector<string> v = {"A2", "A3", "B2", "B1", "C1"};
+    printPermutation(v, 0, 4);
+    //printPermutation("ABC", 0, 2);
     return 0;
 }
